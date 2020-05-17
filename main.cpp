33f61cb596e9cd438b1dbf2acc4cea14abc12b53
Paul <48963940+PaulCooz@users.noreply.github.com>
2020-05-17 14:15:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include "scene.cpp"
 
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    Start();
-
-    Scene scene;
-    QGraphicsView vw(&scene);
-
+    Scene scene;                                                                // Make new scene
     QColor clr;
     clr.SetBlack;
     scene.setBackgroundBrush(clr);
 
+    QGraphicsView vw(&scene);                                                   // Show scene
     vw.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     vw.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
+    vw.setWindowTitle("Maze");
     vw.show();
 
-    a.exec();
+    return a.exec();
 }
