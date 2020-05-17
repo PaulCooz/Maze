9ccf7590b8055e472e2d96ff0c400fe7d54ad8f1
Paul <48963940+PaulCooz@users.noreply.github.com>
2020-05-18 00:50:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     Scene scene;                                                                // Make new scene
-    QColor clr;
-    clr.SetBlack;
+    QColor clr; clr.SetBlack;
     scene.setBackgroundBrush(clr);
 
     QGraphicsView vw(&scene);                                                   // Show scene
