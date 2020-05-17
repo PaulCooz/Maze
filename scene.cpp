e5d2ef71e53e577bd8609d45f0605b7efa16c57b
Paul <48963940+PaulCooz@users.noreply.github.com>
2020-05-18 00:37:19 +0300

    Update scene.cpp

--- scene.cpp
+++ scene.cpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <QApplication>
-#include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QGraphicsItem>
 #include <QGraphicsEllipseItem>
 #include <QGraphicsTextItem>
@@ -11,20 +11,21 @@
 
 #define GoodBorders(x, y) (-1 < (x) && (x) < cR && -1 < (y) && (y) < cC)
 #define Circle(R) QGraphicsEllipseItem(-(R),-(R), 2*(R), 2*(R))
+#define SetColor(item, clr) static_cast<QGraphicsEllipseItem*>(item)->setBrush(clr);
 #define SetWhite setRgb(240, 240, 240, 0)
 #define SetBlack setRgb(  0,   0,   0)
 #define SetRed   setRgb(255, 120, 120, 0)
 #define SetBlue  setRgb(100, 100, 255, 0)
 #define SetPink  setRgb(210,   0, 220, 0)
 #define SetGreen setRgb( 20, 230,  20, 0)
-#define SetCamera(x, y) Scene::setSceneRect(CenterX + x, CenterY + y, 500, 400);
+#define SetCamera(x, y) setSceneRect(CenterX + 2*(x), CenterY + 2*(y), 500, 400);
 
 const int MaxR = 256, MaxC = 256, MaxKeys = 5, FOG = 2;
 const int STP[4][2] = {{ 0,-1},{-1, 0},{ 0,+1},{+1, 0}};
 const char ROAD = '#', END = '%', KEY = '$', EYE = '0', VOID = ' ', GETBONUS = '-';
 
 static char Map[MaxR][MaxC];
-static int cR, cC, PassMazes, cFOG, nkeys, keys;
+static int cR, cC, PassMazes, cFOG, nkeys;
 static int CenterX, CenterY, EndX, EndY;
 
 bool DeadEnd(int x, int y)                                                              // Is deadend
@@ -65,10 +66,10 @@ void MakeNewMaze()
             int cx = Qu[qb][0] + STP[i][0],
                 cy = Qu[qb][1] + STP[i][1];
 
-            if (GoodBorders(cx, cy) && Map[cx][cy] != ROAD &&
+            if (GoodBorders(cx, cy) && Map[cx][cy] != ROAD &&                           // If we can go
                 DeadEnd(cx, cy))
             {
-                Map[cx][cy] = ROAD;
+                Map[cx][cy] = ROAD;                                                     // Add cage
 
                 Qu[qe][0] = cx;
                 Qu[qe][1] = cy;
@@ -101,7 +102,6 @@ void MakeNewMaze()
             }
         }
     }
-    keys = 0;
 }
 
 Scene::Scene(QObject *parent)                                                           // New scene
@@ -109,9 +109,9 @@ Scene::Scene(QObject *parent)                                                   
 {
     srand(time(NULL));
 
-    cR = cC = 10;
-    cFOG = FOG;
+    cR = cC = 8;
     PassMazes = 0;
+    cFOG = FOG;
 
     MakeNewMaze();
     MakeScene();
@@ -119,38 +119,33 @@ Scene::Scene(QObject *parent)                                                   
 
 void Scene::NeedKeys()                                                                  // How many keys are left
 {
-    QPointF P(EndX * 20 - 6, EndY * 20 - 11);
+    QPointF P(EndX * 20 - 7, EndY * 20 - 11);
 
-    if (keys != 0)
-    {
-        QGraphicsItem *crr;
-        crr = Scene::itemAt(P, QTransform());
-        Scene::removeItem(crr);
-    }
+    QGraphicsItem *crr = itemAt(P, QTransform());                                       // Erase old info(if can)
+    if (crr != nullptr) removeItem(crr);
 
-    QGraphicsTextItem *NKeys;
-    NKeys = new QGraphicsTextItem(QString::number(nkeys - keys));
+    QGraphicsTextItem *NKeys;                                                           // Write new
+    NKeys = new QGraphicsTextItem(QString::number(nkeys));
     NKeys->setPos(P);
 
-    Scene::addItem(NKeys);
+    addItem(NKeys);
 }
 
-void Scene::UpdateScene(int x, int y)                                                   // Show visble
+void Scene::UpdateScene(int x, int y)                                                   // Show visible
 {
-    int Zone = cFOG + 1;                                                                // Show road(+remove light)
+    int Zone = cFOG + 1;                                                                // Show road(+remove old light)
     for(int i = -Zone; i <= Zone; i++)
     {
         for(int j = -Zone; j <= Zone; j++)
         {
             if (GoodBorders(x + i, y + j))
             {
+                QColor clr;
+                QList <QGraphicsItem*> L = items(QPointF((x + i) * 20, (y + j) * 20));
                 int dist = abs(i) + abs(j);
+
                 if (dist <= cFOG)
                 {
-                    QColor clr;
-                    QList <QGraphicsItem*> L;
-                    L = Scene::items(QPointF((x + i) * 20, (y + j) * 20));
-
                     if (dist <= cFOG / 2) dist = 0;
                     else
                     {
@@ -161,48 +156,43 @@ void Scene::UpdateScene(int x, int y)                                           
                     if (L.size() == 1)                                                  // Road
                     {
                         clr.SetWhite; clr.setAlpha(255 - dist);
-                        static_cast<QGraphicsEllipseItem*>(L.back())->setBrush(clr);
+                        SetColor(L.back(), clr);
                     }
-                    else if (L.size() == 2)                                             // Road + bonus/key
+                    else if (L.size() == 2)                                             // Road + bonus|key
                     {
                         if (Map[x + i][y + j] == KEY) clr.SetBlue;
                         else if (Map[x + i][y + j] == EYE) clr.SetPink;
-                        else if (i == j && i == 0)
-                        {
-                            clr.SetGreen;
-                        }
+                        else if (i == j && i == 0) clr.SetGreen;
 
                         clr.setAlpha(255 - dist);
-                        static_cast<QGraphicsEllipseItem*>(*L.begin())->setBrush(clr);
+                        SetColor(*L.begin(), clr);
 
-                        clr.SetWhite; clr.setAlpha(255 - dist);
-                        static_cast<QGraphicsEllipseItem*>(L.back())->setBrush(clr);
+                        clr.SetWhite;
+                        clr.setAlpha(255 - dist);
+                        SetColor(L.back(), clr);
                     }
                     else if (L.size() == 3)                                             // Road + end + nkeys
                     {
                         clr.SetRed; clr.setAlpha(255 - dist);
-                        static_cast<QGraphicsEllipseItem*>((*++L.begin()))->setBrush(clr);
+                        SetColor(*++L.begin(), clr);
                     }
                 }
                 else
                 {
-                    QList <QGraphicsItem*> L;
-                    L = Scene::items(QPointF((x + i) * 20, (y + j) * 20));
-
-                    QColor clr; clr.SetWhite;
+                    clr.SetWhite;                                                       // Set void
 
                     if (L.size() == 1)
                     {
-                        static_cast<QGraphicsEllipseItem*>(L.back())->setBrush(clr);
+                        SetColor(L.back(), clr);
                     }
                     else if (L.size() == 2)
                     {
-                        static_cast<QGraphicsEllipseItem*>(*L.begin())->setBrush(clr);
-                        static_cast<QGraphicsEllipseItem*>(L.back())->setBrush(clr);
+                        SetColor(*L.begin(), clr);
+                        SetColor(L.back(), clr);
                     }
                     else if (L.size() == 3)
                     {
-                        static_cast<QGraphicsEllipseItem*>((*++L.begin()))->setBrush(clr);
+                        SetColor(*++L.begin(), clr);
                     }
                 }
             }
@@ -214,9 +204,9 @@ void Scene::UpdateScene(int x, int y)                                           
 
 void Scene::MakeScene()
 {
-    QGraphicsScene::clear();
+    clear();
 
-    int radius = 10;                                                                    // Print map
+    int radius = 10;                                                                    // Set map
     for (int i = 0; i < cR; i++)
     {
         for (int j = 0; j < cC; j++)
@@ -226,48 +216,44 @@ void Scene::MakeScene()
             QGraphicsItem *crr = new Circle(radius);
             crr->setPos(i * 20, j * 20);
 
-            Scene::addItem(crr);                                                        // ---
+            addItem(crr);                                                               // Set road
 
             if (Map[i][j] == ROAD) continue;
 
-            QGraphicsItem *crr1;
-            crr1 = new Circle(radius * (Map[i][j] == END ? 1 : 0.4));
-            crr1->setPos(i * 20, j * 20);
+            crr = new Circle(radius * (Map[i][j] == END ? 1 : 0.4));                    // End|key|bonus
+            crr->setPos(i * 20, j * 20);
 
-            Scene::addItem(crr1);
+            addItem(crr);
         }
     }
 
-    Scene::m_activeItem = new Circle(radius * 0.7);                                     // Add hero
-    Scene::m_activeItem->setPos((cR / 2) * 20, (cC / 2) * 20);
-    Scene::addItem(Scene::m_activeItem);
+    m_activeItem = new Circle(radius * 0.7);                                            // Set hero
+    m_activeItem->setPos((cR / 2) * 20, (cC / 2) * 20);
+    addItem(m_activeItem);
 
-    Scene::NeedKeys();                                                                  // Output nkeys
+    NeedKeys();                                                                         // Output nkeys
 
-    CenterX = -cR * (12 - cR / 2);                                                      // Set camera
-    CenterY = -cC * (10 - cC / 2);
-
+    CenterX = -cR * 18 + PassMazes * 27;                                                // Center the scene
+    CenterY = -cC * 14 + PassMazes * 23;
     UpdateScene(cR / 2, cC / 2);
 }
 
 void Scene::keyPressEvent(QKeyEvent *event)
 {
-    int x = m_activeItem->x() / 20,                                                     // Hero coord and next step
-        y = m_activeItem->y() / 20,
-        io;
-
-    int KeyE = event->key();                                                            // Check press
-    if (KeyE == Qt::Key::Key_W || KeyE == Qt::Key::Key_Up) io = 0;
-    else if (KeyE == Qt::Key::Key_A || KeyE == Qt::Key::Key_Left) io = 1;
-    else if (KeyE == Qt::Key::Key_S || KeyE == Qt::Key::Key_Down) io = 2;
-    else if (KeyE == Qt::Key::Key_D || KeyE == Qt::Key::Key_Right) io = 3;
+    int KeyE = event->key(), io;                                                        // Check press
+    if (KeyE == Qt::Key_W || KeyE == Qt::Key_Up) io = 0;
+    else if (KeyE == Qt::Key_A || KeyE == Qt::Key_Left) io = 1;
+    else if (KeyE == Qt::Key_S || KeyE == Qt::Key_Down) io = 2;
+    else if (KeyE == Qt::Key_D || KeyE == Qt::Key_Right) io = 3;
     else return;
+
+    int x = m_activeItem->x() / 20,                                                     // Hero coord
+        y = m_activeItem->y() / 20;
 
     if (GoodBorders(x + STP[io][0], y + STP[io][1]) &&                                  // If we can go
         Map[x + STP[io][0]][y + STP[io][1]] != VOID)
     {
         m_activeItem->moveBy(STP[io][0] * 20, STP[io][1] * 20);                         // Move hero
-
         x += STP[io][0];
         y += STP[io][1];
 
@@ -276,9 +262,9 @@ void Scene::keyPressEvent(QKeyEvent *event)
             Map[x - STP[io][0]][y - STP[io][1]] = ROAD;
 
             QGraphicsItem *crr;
-            crr = Scene::itemAt((x - STP[io][0]) * 20, (y - STP[io][1]) * 20, QTransform());
+            crr = itemAt((x - STP[io][0]) * 20, (y - STP[io][1]) * 20, QTransform());
 
-            Scene::removeItem(crr);
+            removeItem(crr);
         }
 
         UpdateScene(x, y);
@@ -286,7 +272,7 @@ void Scene::keyPressEvent(QKeyEvent *event)
         switch (Map[x][y])                                                              // Checking the cage
         {
         case END:
-            if (keys == nkeys)
+            if (nkeys == 0)
             {
                 cR++; cC++;
                 PassMazes++;
@@ -300,9 +286,9 @@ void Scene::keyPressEvent(QKeyEvent *event)
             Map[x][y] = GETBONUS;
             break;
         case KEY:
-            keys++;
+            nkeys--;
+            NeedKeys();
             Map[x][y] = GETBONUS;
-            Scene::NeedKeys();
             break;
         }
     }
